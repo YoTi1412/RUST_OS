80c2d9ebd76724a0adc34285f48ef952cb477d6b
YoTi <yasserrafai5@gmail.com>
2023-05-14 23:25:42 +0100

    A Double Fault Handler

--- yoti_os/src/main.rs
+++ yoti_os/src/main.rs
@@ -12,10 +12,12 @@ use yoti_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    yoti_os::init(); // new
+    yoti_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3(); // new
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
