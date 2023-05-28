b6e3bdabdc1890d0ac9b2785417365ea5e6de789
YoTi <yasserrafai5@gmail.com>
2023-05-28 17:35:04 +0100

    Page Faults

--- yoti_os/src/main.rs
+++ yoti_os/src/main.rs
@@ -13,6 +13,18 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     yoti_os::init();
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
+    let ptr = 0x204d26 as *mut u32;
+
+    unsafe { let x = *ptr; }
+    println!("read worked");
+    
+    unsafe { *ptr = 42; }
+    println!("write worked");
 
 
     #[cfg(test)]
