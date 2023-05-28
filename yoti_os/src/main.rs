4d95101d58598ee5ea4eae6355ffeeacd2e9d17b
YoTi <yasserrafai5@gmail.com>
2023-05-28 20:40:03 +0100

    Allocating Frames

--- yoti_os/src/main.rs
+++ yoti_os/src/main.rs
@@ -12,20 +12,26 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use yoti_os::memory::active_level_4_table;
-    use x86_64::VirtAddr;
+//    use x86_64::{structures::paging::Page, VirtAddr};
+//    use yoti_os::memory;
+    use yoti_os::memory::BootInfoFrameAllocator;
 
     println!("Hello World{}", "!");
     yoti_os::init();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+//    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+//    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+//    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
-    }
+//    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+//    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+//    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+//    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
     // as before
     #[cfg(test)]
