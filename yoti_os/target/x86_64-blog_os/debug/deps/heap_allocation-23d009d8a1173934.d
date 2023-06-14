5d268665ffa1819d6df39588d540d0b3fa4b2359
YoTi <yasserrafai5@gmail.com>
2023-06-14 16:11:18 +0100

    adding a test, heap allocation

---
+++ yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-23d009d8a1173934.d
@@ -0,0 +1,5 @@
+/Users/info/Desktop/RUST_OS/yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-23d009d8a1173934.rmeta: tests/heap_allocation.rs
+
+/Users/info/Desktop/RUST_OS/yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-23d009d8a1173934.d: tests/heap_allocation.rs
+
+tests/heap_allocation.rs:
---
+++ yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-7c27f86add10acc0.d
@@ -0,0 +1,5 @@
+/Users/info/Desktop/RUST_OS/yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-7c27f86add10acc0: tests/heap_allocation.rs
+
+/Users/info/Desktop/RUST_OS/yoti_os/target/x86_64-blog_os/debug/deps/heap_allocation-7c27f86add10acc0.d: tests/heap_allocation.rs
+
+tests/heap_allocation.rs:
